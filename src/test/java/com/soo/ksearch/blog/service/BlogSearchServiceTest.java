2532a95df4be6695da68ce1462acddd59a14b995
soojung930130 <soojung930130@email.com>
2023-03-23 11:25:28 +0900

    Test 및 Build 실패 이슈 해결

--- src/test/java/com/soo/ksearch/blog/service/BlogSearchServiceTest.java
+++ src/test/java/com/soo/ksearch/blog/service/BlogSearchServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
@@ -19,11 +20,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DBRider
 @SpringBootTest
 @ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
 class BlogSearchServiceTest {
 
     @Autowired
     private BlogSearchService blogSearchService;
-
 
 
     @Nested
